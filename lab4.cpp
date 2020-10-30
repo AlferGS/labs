c492cc7a2910aed688ae7c5ca0c7116e4facd7d9
Alfer <71255030+AlferGS@users.noreply.github.com>
2020-10-30 09:47:29 +0300

    Add files via upload

--- lab4.cpp
+++ lab4.cpp
@@ -11,88 +11,18 @@ protected:
 	int tank_size;
 
 public:
-	Transport(string s = "empty", double d = 0.0, int i = 0 )
-		:body_material(s), engine_volume(d), tank_size(i)
+	Transport(int tanksize = 0, double engine_vol = 0.0, string body_mat = "")
 	{
-//		if (this->body_material == "empty" || this-> body_material == "" ) { this->body_material = s; }
-	//	if (this->engine_volume == 0.0 || this->engine_volume == NULL) { this->engine_volume = d; }
-		//if (this->tank_size == 0 || this->tank_size == NULL) { this->tank_size = i; }
-		//cout <<this<< " - Transport(string s, double d, int i)" << endl;
+		body_material = body_mat;
+		engine_volume = engine_vol;
+		tank_size = tanksize;
 	}
 	~Transport()
 	{
 		//cout <<this << " - ~Transport()" << endl;
 	}
 
-
-
-	virtual int int_check(string s)
-	{
-		int i = 0, n = 0;
-		while (s[i])
-		{
-			if (int(s[i]) >= 48 && s[i] <= 57)
-			{
-				n = n * 10 + int(s[i] - 48);
-			}
-			else
-			{
-				cin.clear();
-				cin.ignore(54321, '\n');
-				cout << "error input. try again" << endl;
-				cin >> s;
-				return int_check(s);
-			}
-			i++;
-		}
-		return n;
-	}
-
-	virtual double double_check(string s)
-	{
-		int i = 0, flg = 0;
-		double in = 0, dn = 0;
-		while (s[i])
-		{
-			if (double(s[i]) >= 48 && s[i] <= 57)
-			{
-				i++;
-				continue;
-			}
-			else if (char(s[i]) == '.' && flg == 0)
-			{
-				flg = 1;
-				i++;
-				continue;
-			}
-			else
-			{
-				cin.clear();
-				cin.ignore(54321, '\n');
-				cout << "error input. try again" << endl;
-				cin >> s;
-				return double_check(s);
-			}
-		}i = 0;
-		while (s[i] != '.')
-		{
-			in = in * 10 + double(s[i] - 48);
-			i++;
-		}
-		int j = i;
-		i++;
-		while (s[i] != '\0')
-		{
-			dn = dn + double(s[i] - 48)/(pow(10,i-j));
-			i++;
-		}
-		in = in + dn;
-		return in;
-	}
-
-
-
-	virtual void input_Transport()
+	/*virtual void input_Transport()
 	{
 		string dtmp, itmp;
 		if (this->body_material == "empty" || this->body_material == "") {
@@ -100,27 +30,28 @@ public:
 			cout << "Enter body material - ";
 			cin >> this->body_material;
 		}
-		if (this->engine_volume == 0.0 || this->engine_volume == NULL) {
+		if (this->engine_volume == NULL) {
 			cout << "Enter engine volume - ";
 			cin >> dtmp;
 			this->engine_volume = double_check(dtmp);
 		}
-		if (this->tank_size == 0 || this-> tank_size == NULL) {
+		if (this-> tank_size == NULL) {
 			cout << "Enter tank size - ";
 			cin >> itmp;
 			this->tank_size = int_check(itmp);
 		}
-	}
+	}*/
 
 	virtual void T_show()
 	{
 		cout << endl;
+		flagpoint = 0;
 		if (flagpoint == 0) {
-			cout << "Show:" << endl;
+			cout << "Transport Show:" << endl;
 			cout << "body_material - " << this->body_material << endl;;
 			cout << "endgine_volume - " << this->engine_volume << endl;
 			cout << "tank_size - " << this->tank_size << endl;
-			flagpoint++;
+			//flagpoint++;
 		}
 		else 
 		{
@@ -128,21 +59,25 @@ public:
 		}
 	}
 
+	virtual void set_body_material(string s) { this->body_material = s; }
+	virtual void set_engine_volume(double d) {  this->engine_volume = d; }
+	virtual void set_tank_size(int i) {  this->tank_size = i; }
+
 	virtual string get_body_material()	{	return this->body_material;	}
 	virtual double get_engine_volume()	{	return this->engine_volume;	}
 	virtual int get_tank_size()	{	return this->tank_size;	}
 };
 
-class Ground_transport: virtual public Transport
+class Ground_transport: public Transport
 {
 protected:
 	int num_of_wheels;
 
 public:
-	Ground_transport(string s = "empty", double d =0.0 , int i = 0, int NoW = 0)
-		:Transport(s,d,i)
+	Ground_transport(int NoW = 0, int tanksize = 0, double engine_vol = 0.0, string body_mat = "")
+		:Transport(tanksize, engine_vol, body_mat)
 	{
-		this->num_of_wheels = NoW;
+		num_of_wheels = NoW;
 		//cout << this << "Ground_Transport(int NoW)" << endl;
 	}
 	~Ground_transport()
@@ -150,17 +85,15 @@ public:
 		//cout << this << "~Ground_Transport()" << endl;
 	}
 
-
-
-	void input_GT() 
-	{
-		string tmpint;
-		cout << "input GT" << endl;
-		cout << "Enter num_of_wheels - ";
-		cin >> tmpint;
-		this->num_of_wheels = int_check(tmpint);
-		this->input_Transport();
-	}
+	//void input_GT() 
+	//{
+	//	string tmpint;
+	//	cout << "input GT" << endl;
+	//	cout << "Enter num_of_wheels - ";
+	//	cin >> tmpint;
+	//	this->num_of_wheels = int_check(tmpint);
+	//	this->input_Transport();
+	//}
 
 	void GT_show()
 	{
@@ -181,30 +114,27 @@ protected:
 	string gearbox;
 
 public:
-	Car(string s = "empty", double d = 0.0, int i = 0, int NoW = 0, int T_capacity = 0, string numgear = "empty")
-		:Ground_transport(s, d, i, NoW)
+	Car(int T_capacity = 0, string numgear = "empty", int NoW = 0, int tanksize = 0, double engine_vol = 0.0, string body_mat = "")
+		:Ground_transport(NoW, tanksize, engine_vol, body_mat)
 	{
-		this->trunk_capacity = T_capacity;
-		this->gearbox = numgear;
-		//cout << "Car()" << endl;
+		trunk_capacity = T_capacity;
+		gearbox = numgear;
 	}
 	~Car() { 
 		//cout << "~Car()" << endl;
 	}
 
-
-
-	void input_Car()
-	{
-		string inttmp;
-		cout << "input_Car:" << endl;
-		cout << "Enter trunk capacity - ";
-		cin >> inttmp;
-		this->trunk_capacity = int_check(inttmp);
-		cout << "Enter gearbox - ";
-		cin >> this->gearbox;
-		this->input_GT();
-	}
+	//void input_Car()
+	//{
+	//	string inttmp;
+	//	cout << "input_Car:" << endl;
+	//	cout << "Enter trunk capacity - ";
+	//	cin >> inttmp;
+	//	this->trunk_capacity = int_check(inttmp);
+	//	cout << "Enter gearbox - ";
+	//	cin >> this->gearbox;
+	//	this->input_GT();
+	//}
 
 	void Car_show()
 	{
@@ -219,27 +149,22 @@ public:
 	string get_geatbox() { return this->gearbox; }
 };
 
-class Water_Transport: virtual public Transport
+class Water_Transport: public Transport
 {
 protected:
 	int displacement;
 
 public:
-
-	Water_Transport(string s = "empty", double d = 0.0, int i = 0, int displacemnt = 0)
-		:Transport(s, d, i)
+	Water_Transport(int displacemnt, int tanksize, double engine_vol, string body_mat )	:Transport(tanksize, engine_vol, body_mat)
 	{
-		this->displacement = displacemnt;
-		//cout << "Water_Transport()" << endl;
+		displacement = displacemnt;
 	}
 
 	~Water_Transport() { 
 		//cout << "~Water_Transport()" << endl; 
 	}
 
-
-
-	void input_Water_Transport()
+	/*void input_Water_Transport()
 	{
 		string int_tmp;
 		cout << "input_Water_Transport:" << endl;
@@ -247,7 +172,7 @@ public:
 		cin >> int_tmp;
 		this->displacement = this->int_check(int_tmp);
 		this->input_Transport();
-	}
+	}*/
 
 	void WT_show()
 	{
@@ -266,28 +191,25 @@ protected:
 	double angle_at_the_roll;					// угол наклона при крене
 
 public:
-	Motor_Boat(string s = "emty", double d = 0.0, int i = 0, int displacemnt = 0, double aatr = 0.0)
-		:Water_Transport(s,d,i,displacemnt)
+	//Motor_Boat() {}
+	Motor_Boat(double aatr, int displacemnt, int tanksize, double engine_vol, string body_mat)	:Water_Transport(displacemnt, tanksize, engine_vol, body_mat)
 	{
-		this->angle_at_the_roll = aatr;
-		//cout << "Motor_Boat()" << endl;
+		angle_at_the_roll = aatr;
 	}
 	
 	~Motor_Boat() { 
 		//cout << "~Motor_Boat()" << endl;
 	}
 
-
-
-	void input_Motor_Boat()
-	{
-		string doubletmp;
-		cout << "input_Motor_Boat:" << endl;
-		cout << "Enter angle at the roll - ";
-		cin >> doubletmp;
-		this->angle_at_the_roll = double_check(doubletmp);
-		this->input_Water_Transport();
-	}
+	//void input_Motor_Boat()
+	//{
+	//	string doubletmp;
+	//	cout << "input_Motor_Boat:" << endl;
+	//	cout << "Enter angle at the roll - ";
+	//	cin >> doubletmp;
+	//	this->angle_at_the_roll = double_check(doubletmp);
+	//	this->input_Water_Transport();
+	//}
 
 	void MB_show()
 	{
@@ -307,26 +229,23 @@ protected:
 	bool water_cannon;
 
 public:
-	Car_Amphib(string s = "empty", double d = 0.0, int i = 0, int displacemnt = 0, double aatr = 0.0, int NoW = 0, int T_capacity = 0, string numgear = "empty", bool watr_cannon = false)
-		:Motor_Boat(s, d, i, displacemnt, aatr), Car(s, d, i, NoW, T_capacity, numgear)
+	Car_Amphib(bool watr_cannon = false, string numgear = "empty", int T_capacity = 0, int NoW = 0, double aatr = 0.0, int displacemnt = 0, int tanksize = 0, double engine_vol = 0.0, string body_mat = "")
+		:Motor_Boat(aatr, displacemnt, tanksize, engine_vol, body_mat), Car(T_capacity, numgear, NoW, tanksize, engine_vol, body_mat)
 	{
 		this->water_cannon = watr_cannon;
-		//cout << "Car_Amphib()" << endl;
 	}
 	~Car_Amphib() { 
 		//cout << "~Car_Amphib()" << endl;
 	}
 
-
-
-	virtual void input_Car_Amphib()
-	{
-		cout << "input Car_Amphib:" << endl;
-		cout << "Have you water cannon - ";
-		cin >> this->water_cannon;
-		this->input_Car();
-		this->input_Motor_Boat();
-	}
+	//virtual void input_Car_Amphib()
+	//{
+	//	cout << "input Car_Amphib:" << endl;
+	//	cout << "Have you water cannon - ";
+	//	cin >> this->water_cannon;
+	//	this->input_Car();
+	//	this->input_Motor_Boat();
+	//}
 
 	void CA_show()
 	{
@@ -339,15 +258,154 @@ public:
 	bool get_water_cannon() { return this->water_cannon; }
 };
 
+
+int int_check(string s)
+{
+	int i = 0, n = 0;
+	while (s[i])
+	{
+		if (int(s[i]) >= 48 && s[i] <= 57)
+		{
+			n = n * 10 + int(s[i] - 48);
+		}
+		else
+		{
+			cin.clear();
+			cin.ignore(54321, '\n');
+			cout << "error input. try again" << endl;
+			cin >> s;
+			return int_check(s);
+		}
+		i++;
+	}
+	return n;
+}
+
+
+double double_check(string s)
+{
+	int i = 0, flg = 0;
+	double in = 0, dn = 0;
+	while (s[i])
+	{
+		if (int(s[i]) >= 48 && int(s[i]) <= 57 && s[i] != '\0')
+		{
+			i++;
+			continue;
+		}
+		else if (char(s[i]) == '.' && flg == 0)
+		{
+			flg = 1;
+			i++;
+			continue;
+		}
+		else
+		{
+			cin.clear();
+			cin.ignore(54321, '\n');
+			cout << "error input. try again" << endl;
+			cin >> s;
+			return double_check(s);
+		}
+	}
+	int j = i;
+	i = 0; flg = 0;
+	while (flg == 0)
+	{
+		if (s[i] == '.')
+		{
+			flg = 1;
+			break;
+		}
+		if (int(s[i]))
+		{
+			in = in * 10 + double(double(s[i]) - 48);
+			i++;
+		}
+		else
+		{
+			break;
+		}
+	}
+	int x = i;
+	i++;
+	while (flg == 1)
+	{
+		if (i < j) {
+			dn = dn + double(double(s[i]) - 48) / (pow(10, i - x));
+			i++;
+		}
+		else
+		{
+			flg = 0;
+		}
+	}
+	in = in + dn;
+	return in;
+}
+
+
 int main(void)
 {
-	Car_Amphib a, b("qwe", 1.2, 3, 4, 5.6, 7, 8, "asd", true), c;
-	a.input_Car_Amphib();
-	cout << endl << endl << endl;
-	a.CA_show();
-	cout << endl << endl << endl;
-	b.CA_show();
-	cout << endl << endl << endl;
-	c.CA_show();
-	a.T_show();
+	bool water_gun;
+	int tank_size, num_of_wheels, displacement, trunk_capacity;
+	double engine_volume, angle_at_the_roll;
+	string body_material, gearbox;
+
+	string itmp, dtmp;
+
+	cout << "Enter angle at de roll(doub) - ";
+	cin >> dtmp;
+	angle_at_the_roll = double_check(dtmp);
+
+	cout << endl << "Enter displacement(int) - ";
+	cin >> itmp;
+	displacement = int_check(itmp);
+
+	cout << endl << "Enter body_material(str) - ";
+	cin >> body_material;
+
+	cout << endl << "Enter engine_volume(doub) - ";
+	cin >> dtmp;
+	engine_volume = double_check(dtmp);
+
+	cout << endl << "Enter tank_size(int) - ";
+	cin >> itmp;
+	tank_size = int_check(itmp);
+
+	cout << "Enter gearbox(str) - ";
+	cin >> gearbox;
+
+	cout << endl << "Enter trunk_capacity(int) - ";
+	cin >> itmp;
+	trunk_capacity = int_check(itmp);
+
+	cout << endl << "Enter num_of_wheels(int) - ";
+	cin >> itmp;
+	num_of_wheels = int_check(itmp);
+
+	cout << endl << "Enter water_gun(bool) - ";
+	cin >> water_gun;
+
+	cout << "Input end" << endl;
+
+	// Задание 3)
+	Car_Amphib CA(water_gun, gearbox, trunk_capacity, num_of_wheels, angle_at_the_roll, displacement, tank_size, engine_volume, body_material);
+
+
+	Motor_Boat MB(angle_at_the_roll, displacement, tank_size, engine_volume, body_material);
+	Car C(trunk_capacity, gearbox, num_of_wheels, tank_size, engine_volume, body_material);
+	
+	MB.MB_show();
+	cout << endl;
+
+	//MB.T_show();
+	C.Car_show();
+
 }
+
+
+
+
+
+
