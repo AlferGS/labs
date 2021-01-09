0984e0490785b1176accf3416ca6b617d5dae6ef
Alfer <71255030+AlferGS@users.noreply.github.com>
2021-01-09 04:14:52 +0300

    Add files via upload

---
+++ lab9/main.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main(void)
+{
+	string str1, str2;
+	str1 = "~~~";
+	str2 = "*22";
+
+	cout << int(3/2) << endl;
+	//cout << strcmp(str1.c_str(), "~~~") << endl;
+	/*cout << str1 << endl << str2 << endl;
+
+	char ch1[5], ch2[5], t_ch1[5], t_ch2[5];
+	strcpy_s(ch1, str1.c_str());
+	strcpy_s(ch2, str2.c_str());
+
+	for (int i = 1, j = 0; i < 5; i++, j++)
+	{
+		t_ch1[j] = ch1[i];
+	}
+	for (int i = 1, j = 0; i < 5; i++, j++)
+	{
+		t_ch2[j] = ch2[i];
+	}
+
+	strcpy_s(ch1, t_ch1);
+	strcpy_s(ch2, t_ch2);
+
+	cout << "char" << endl;
+	cout << ch1 << endl << ch2 << endl;
+
+	cout << "Int" << endl;
+	int num1, num2;
+	num1 = atoi(ch1);
+	num2 = atoi(ch2);
+	cout << num1 << endl << num2 << endl;
+*/
+
+
+
+
+
+	cin >> str1;
+}
